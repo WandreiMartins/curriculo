8613261515300dea504f1d113575236dd0fcba14
Wandrei Martins <wandrei.martins@gmail.com>
2021-05-03 14:47:36 -0300

    box

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const msg = document.querySelector("#msg")
-window.onload = setTimeout(function(){
-    msg.style.left = "-100%";
-    msg.style.transition = '0.5s ease-in-out'
-}, 2000)
+// const msg = document.querySelector("#msg")
+// window.onload = setTimeout(function(){
+//     msg.style.left = "-100%";
+//     msg.style.transition = '0.5s ease-in-out'
+// }, 2000)
     
    
+let fechar = document.querySelector("[data-fechar]")
+let seta = document.querySelector("#abrir")
+let btn = document.querySelector("#fechar")
+
+fechar.onclick = ()=>{
+    btn.classList.add('fchr')
+    seta.classList.remove('fchr')
+}
+seta.onclick = ()=>{
+    btn.classList.remove('fchr')
+    seta.classList.add('fchr')
+}
 
   
